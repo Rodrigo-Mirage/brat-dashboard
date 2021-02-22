4c8752b76a2e832c2a7eab83745ae1ab533eda7d
Rodrigo-Mirage <mirage4d@hotmail.com>
2021-02-22 20:31:38 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,8 @@ let publicPath = process.env.NODE_ENV === 'production' ? 'light-blue-vue-admin/'
 module.exports = {
   publicPath,
   productionSourceMap: false,
+  baseUrl: '0.0.0.0',
+  devServer: {
+    host: 'BrAT-Testes'
+  }
 };
